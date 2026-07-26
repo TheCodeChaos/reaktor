240e69241d6b1ac7ac761668fdabe6f2b5875ffc
Jitendra Verma <jitendravjh@gmail.com>
2026-07-26 08:36:20 +0530

    Include the base path in rss item links (#1)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -16,7 +16,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.id}/`,
+      link: `blog/${post.id}/`,
     })),
   });
 }
